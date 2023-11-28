9a7c1dd1ab6f09940cdb626fa310a7126f351fc0
Michael McGinn <mcginnm7@miamioh.edu>
2023-11-28 18:43:15 -0500

    praying this works

--- routes/customPizza.js
+++ routes/customPizza.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     // Fetch and display customization options
     fetch('/api/customizations')
-        .then(response => response.json())
-        .then(data => displayCustomizations(data));
+        .then((response) => response.json())
+        .then((data) => displayCustomizations(data));
 
     // Add event listener for the Add Custom Pizza button
-    document.getElementById('addCustomPizza').addEventListener('click', addCustomPizzaToCart);
+    document
+        .getElementById('addCustomPizza')
+        .addEventListener('click', addCustomPizzaToCart);
 });
 
 function displayCustomizations(data) {
     let content = '';
-    data.forEach(c => {
+    data.forEach((c) => {
         content += `<div>
                         <input type="checkbox" id="customization-${c.id}" name="customizations" value="${c.id}">
                         <label for="customization-${c.id}">${c.name}</label>
@@ -20,18 +22,27 @@ function displayCustomizations(data) {
 }
 
 function addCustomPizzaToCart() {
-    const checkedBoxes = document.querySelectorAll('input[name="customizations"]:checked');
-    const customizations = Array.from(checkedBoxes).map(cb => parseInt(cb.value));
+    const checkedBoxes = document.querySelectorAll(
+        'input[name="customizations"]:checked'
+    );
+    const customizations = Array.from(checkedBoxes).map((cb) =>
+        parseInt(cb.value)
+    );
 
-    axios.post('/api/cart', {
-        product_id: id,
-        quantity: 1,
-        customizations: customizations
-    }).then(response => {
-        // Update cart count and reset selections
-        document.getElementById('cart').textContent = `Cart(${response.data.cartCount})`;
-        checkedBoxes.forEach(cb => cb.checked = false);
-    }).catch(error => {
-        console.error('Error adding custom pizza to cart:', error);
-    });
+    axios
+        .post('/api/cart', {
+            product_id: id,
+            quantity: 1,
+            customizations,
+        })
+        .then((response) => {
+            // Update cart count and reset selections
+            document.getElementById(
+                'cart'
+            ).textContent = `Cart(${response.data.cartCount})`;
+            checkedBoxes.forEach((cb) => (cb.checked = false));
+        })
+        .catch((error) => {
+            console.error('Error adding custom pizza to cart:', error);
+        });
 }
