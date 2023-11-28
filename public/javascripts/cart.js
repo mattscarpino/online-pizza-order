77cb74b1abe900d4f289dd40e62af224d4cf9b09
mattscarpino <scarpimc@miamioh.edu>
2023-11-28 17:25:50 -0500

    part 7 done, made part 5 better

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -2,9 +2,14 @@
 // eslint-disable-next-line no-unused-vars
 async function addItem(id, itemCount) {
     // eslint-disable-next-line no-unused-vars
+
+    let num = document.getElementById('howMany' + id);
+    let value = num.value;
+
+
     const result = await axios.post('/api/cart', {
         product_id: id,
-        quantity: 1
+        quantity: value
     });
     itemCount = req.data.cartCount;
     return itemCount;
